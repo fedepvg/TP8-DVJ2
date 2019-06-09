426577d4a376cfee2318ea2f87ade4fa7ef740e7
fedepvg <federicopvg@gmail.com>
2019-06-09 14:35:25 -0300

    player shoot improved

--- TP8-DVJ2/Assets/Scripts/Bullet.cs
+++ TP8-DVJ2/Assets/Scripts/Bullet.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float Speed;
+    Transform Bounds;
+
+    private void Start()
+    {
+        Bounds = GameObject.Find("Bounds").transform;
+    }
 
     void Update()
     {
         Vector3 pos = transform.position;
         pos += new Vector3(0, Speed*Time.deltaTime, 0);
         transform.position = pos;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag=="Bounds")
+            Destroy(gameObject);
     }
 }
