ff1fedf525f9a696185f791f45579d1a804a5d0c
fedepvg <federicopvg@gmail.com>
2019-06-10 17:20:16 -0300

    parallax added

---
+++ TP8-DVJ2/Assets/Scripts/Gameplay/Parallax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    Bounds SpriteBounds;
+    public float ParallaxSpeed;
+    private Vector3 StartPosition;
+
+    void Start()
+    {
+        SpriteBounds = GetComponent<SpriteRenderer>().bounds;
+        StartPosition = transform.position;
+    }
+    
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * ParallaxSpeed, SpriteBounds.size.y);
+        transform.position = StartPosition - Vector3.up * newPosition;
+    }
+}
